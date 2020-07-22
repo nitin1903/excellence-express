f522a88d7171e4ab0c12b4923c49dbcabded2ce7
Nitin <ntngoyal19@gmail.com>
2020-07-22 12:21:01 +0530

    cleaned file

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,14 @@ const User = require('../models/user')
 const AccessToken = require('../models/access-token')
 const Address = require('../models/address')
 
+//user data validators
+
 async function validateData(req, res, next){
   const user = req.body
-  const a = await isUniqueUserName(user.user_name)
-  const b = await isEmailUnique(user.email)
-  const c = await passworMatch(user.password, user.confPassword)
-  if(!a || !b || !c) {
+  const uniqueUser = await isUniqueUserName(user.user_name)
+  const uniqueEmail = await isEmailUnique(user.email)
+  const passwordMatch = await isPassworMatch(user.password, user.confPassword)
+  if(!uniqueUser || !uniqueEmail || !passworMatch) {
        return res.sendStatus(400)
   }
   
@@ -20,48 +22,35 @@ async function validateData(req, res, next){
 
 async function isUniqueUserName(user_name) {
   let result = await User.findOne({user_name})
-  if(result == null) {
-      return true
-  }
-  console.log("username not unique")
-  return false
-  //return (result == null)
+  
+  return (result == null)
 }
 
 async function isEmailUnique(email) {
   let result = await User.findOne({email})
-  if(result == null) {
-      return true
-  }
-  console.log("email not unique")
-  return false
- // return (result == null)
+  
+  return (result == null)
 }
 
-function passworMatch(password, confPassword) {
-  if(password == confPassword){
-      return true
-  }
-  console.log("password does not match")
-  return false
-  //return password === confPasword
+function isPassworMatch(password, confPassword) {  
+  return password === confPasword
 }
 
-// end of validateData middleware
-
+//Address Validation
 function validateAddress(req, res, next) {
-  if(req.body.pin_code.length > 6){
-    return res.sendStatus(401)
-  }
-  req.body.pin_code = Number(req.body.pin_code)
+  const pin_code = req.body.pin_code.length
+  const phone_number = req.body.phone_number.length
 
-  if(req.body.phone_number.length > 10) {
-    return res.sendStatus(401)
+  if(pin_code.length != 6 || phone_number.length != 10){
+    return res.sendStatus(400)
   }
-  req.body.phone_number.length = Number(req.body.phone_number.length)
+
+  req.body.pin_code = Number(pin_code)
+  req.body.phone_number.length = Number(phone_number)
   next()
 }
 
+//Token Validation
 async function validateToken(req, res, next) {
   const token = req.get('authorization').split(' ')[1]
   console.log(`token ${token}`)
@@ -86,18 +75,19 @@ async function validateToken(req, res, next) {
   }
 }
 
+//token generation
 function generateToken() {
-  const d = new Date
-  return crypto.createHash('md5').update(d.getTime().toString()).digest('hex')
+  const date = new Date
+  return crypto.createHash('md5').update(date.getTime().toString()).digest('hex')
 }
 
 async function saveToken(token, id) {
-  const d = new Date()
-  d.setHours(d.getHours() + 1)
+  const date = new Date()
+  date.setHours(date.getHours() + 1)
   const accessToken = new AccessToken({
     user_id : id,
     access_token : token,
-    expiry : d
+    expiry : date
   })
 
   try{
@@ -110,16 +100,16 @@ async function saveToken(token, id) {
 }
 
 router.post('/register', validateData, async (req, res) => {
-  const user = req.body
-  const u = new User({
-      user_name: user.user_name,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      password: crypto.createHash('md5').update(user.password).digest('hex')
+  const userData = req.body
+  const user = new User({
+      user_name: userData.user_name,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      password: crypto.createHash('md5').update(userData.password).digest('hex')
   })
   try{
-      let result = await u.save()
+      let result = await user.save()
       console.log(result)
       res.status(200)
       return res.json(result)
@@ -133,14 +123,14 @@ router.post('/register', validateData, async (req, res) => {
 router.post('/login', async (req, res) => {
   const user_name = req.body.user_name
   const password = crypto.createHash('md5').update(req.body.password).digest('hex')
-  console.log(`password: ${password}`)
+  
   try{
       let result = await User.findOne({user_name})
-      console.log(`login result ${result}`)
+      
       if(result == null) {
           return res.sendStatus(500)
       }
-      console.log(`result password: ${result.password}`)
+      
       if(result.password == password) {
           const token = generateToken()
           console.log(`token is: $token`)
@@ -149,8 +139,9 @@ router.post('/login', async (req, res) => {
           return res.json({"access_token" : token})
           
       }
+
       return res.sendStatus(403)
-  } catch(err) {
+    } catch(err) {
       console.log(err)
       return res.sendStatus(500)
   }
@@ -160,25 +151,26 @@ router.post('/login', async (req, res) => {
 router.get('/get', validateToken, async (req, res) => {
   try{
     const result = await User.findById(res.locals.result.user_id).populate('addresses')
+
     res.status(200)
     return res.json(result)
-  } catch(err){}
-  return res.sendStatus(500)
+  } catch(err){
+    return res.sendStatus(500)
+  }
 })
 
 router.put('/delete', validateToken, async (req, res) => {
   try{
-      let result = await User.deleteOne({_id: res.locals.accessToken})
-      console.log(`delete result ${result}`)
+      let result = await User.deleteOne({_id: res.locals.result.user_id})
+      
       return res.sendStatus(200)
   } catch(err) {
-      console.log(err)
       return res.sendStatus(500)
   }
 })
 
 router.post('/address', validateToken, validateAddress, async (req, res) => {
-  const ad = new Address({
+  const address = new Address({
     user_id : res.locals.result.user_id,
     address: req.body.address,
     city: req.body.city,
@@ -188,8 +180,7 @@ router.post('/address', validateToken, validateAddress, async (req, res) => {
   })
 
   try{
-    let result = await ad.save()
-    console.log(result)
+    let result = await address.save()
     let result2 = await User.findOneAndUpdate({_id: result.user_id},
       {"$push": {addresses: result._id}})
     return res.sendStatus(200)
