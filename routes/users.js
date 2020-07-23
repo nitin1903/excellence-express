f001b7ccd213910bd9e5c66f5afa5b63d61cd19c
Nitin <ntngoyal19@gmail.com>
2020-07-23 16:02:13 +0530

    added middleware

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,14 @@ var express = require('express');
 const crypto = require('crypto')
 const jwtValidate = require('express-jwt')
 
-const secret = require('../config').secret
 const User = require('../models/user')
 const Address = require('../models/address')
 const generateToken = require('../utils/token-generator')
+const verifyToken = require('../middleware/validate-jwt')
 
 var router = express.Router();
 
-router.use(jwtValidate({secret, algorithms: ['HS256']}))
-router.use((err, req, res, next) => {
-  if(req.path === '/login' || req.path === '/register'){
-    return next()
-  }
-  if(err.name === 'UnauthorizedError'){
-    return res.status(401).send('invalid token..')
-  }
-})
-
 router.post('/register', async (req, res) => {
-  if(req.user){
-    return res.status(200).json({message: "you are logged in.."})
-  }
   const userData = req.body
   if(userData.password != userData.confPassword) {
     return res.status(400).json({message: "password does not match"})
@@ -36,9 +23,6 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  if(req.user){
-    return res.status(200).json({message: "you are logged in.."})
-  }
   try{
     const user = await User.getUser(req.body)
     if(user == null){
@@ -51,7 +35,7 @@ router.post('/login', async (req, res) => {
   }
 })
 
-router.post('/address', async (req, res) => {
+router.post('/address', verifyToken, async (req, res) => {
   try{
     const result = await Address.addAddress(req.user.user_id, req.body)
     await User.addAddress(result.user_id, result._id)
@@ -61,7 +45,7 @@ router.post('/address', async (req, res) => {
   }
 })
 
-router.get('/get', async (req, res) => {
+router.get('/get', verifyToken, async (req, res) => {
   try{
     const result = await User.findById(req.user.user_id).populate('addresses')
     return res.status(200).json(result)
@@ -70,10 +54,9 @@ router.get('/get', async (req, res) => {
   }
 })
 
-
-router.put('/delete', async (req, res) => {
+router.put('/delete', verifyToken, async (req, res) => {
   try{
-    let result = await User.deleteOne({_id: req.juser.user_id})      
+    let result = await User.deleteOne({_id: req.user.user_id})      
     return res.status(200).json({message: "user deleted"})
   } catch(err) {
     return res.status(500).json({error: "internal server error"})
